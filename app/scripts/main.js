ded09cebbdc508d1cef828f3c3b9bbbb81f6d9f5
sycohuesca <ajlucea@hotmail.com>
2015-03-05 07:27:44 +0100

    Añadimos validar CIF.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -37,6 +37,13 @@ $("#miformulario").validate({
                $('#nif_cif').val().toUpperCase();
             return "nif";
         }
+    },
+    cifES: function (){
+    	// Comprobamos si esta selecionado empresa y comprobamos que sea un cif valido.
+    	    if ($("#empresa").is(":checked")){
+               $('#nif_cif').val().toUpperCase();
+            return "cifES";
+        }
     }
     },
     nom_emp:"required",
@@ -282,6 +289,46 @@ jQuery.validator.addMethod('nif', function(value, element) {
 		return false;
 	}
 }, 'Introduce un NIF válido.');
+
+// Añadimos el metodo comprobar CIF español.
+jQuery.validator.addMethod( "cifES", function ( value, element ) {
+ "use strict";
+  
+ var sum,
+  num = [],
+  controlDigit;
+  
+ value = value.toUpperCase();
+  
+ // Basic format test
+ if ( !value.match( '((^[A-Z]{1}[0-9]{7}[A-Z0-9]{1}$|^[T]{1}[A-Z0-9]{8}$)|^[0-9]{8}[A-Z]{1}$)' ) ) {
+  return false;
+ }
+  
+ for ( var i = 0; i < 9; i++ ) {
+  num[ i ] = parseInt( value.charAt( i ), 10 );
+ }
+  
+ // Algorithm for checking CIF codes
+ sum = num[ 2 ] + num[ 4 ] + num[ 6 ];
+ for ( var count = 1; count < 8; count += 2 ) {
+  var tmp = ( 2 * num[ count ] ).toString(),
+   secondDigit = tmp.charAt( 1 );
+   
+  sum += parseInt( tmp.charAt( 0 ), 10 ) + ( secondDigit === '' ? 0 : parseInt( secondDigit, 10 ) );
+ }
+  
+ // CIF test
+ if ( /^[ABCDEFGHJNPQRSUVW]{1}/.test( value ) ) {
+  sum += '';
+  controlDigit = 10 - parseInt( sum.charAt( sum.length - 1 ), 10 );
+  value += controlDigit;
+  return ( num[ 8 ].toString() === String.fromCharCode( 64 + controlDigit ) || num[ 8 ].toString() === value.charAt( value.length - 1 ) );
+ }
+  
+ return false;
+  
+}, "Introduzca un CIF correcto." );
 
 // mensajes de validacion español.
 (function ($) {
